3b7a61eacf27e89827826e918df630472ecdef92
Mini Mathachan <72683924+minialappatt@users.noreply.github.com>
2024-05-08 02:09:39 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "c5f881b4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter first number11\n",
+      "enter second number2\n",
+      "sum of 2 numbers 13\n",
+      "diffrence between 2 numbers is  9\n",
+      "product of 2 numbers 22\n",
+      "quotient of 2 numbers is 5.5\n"
+     ]
+    }
+   ],
+   "source": [
+    "def addition(num1,num2):\n",
+    "    print(\"sum of 2 numbers\",num1+num2)\n",
+    "    \n",
+    "def subtraction(num1,num2):\n",
+    "    print(\"diffrence between 2 numbers is \",num1-num2)\n",
+    "    \n",
+    "def multiplication(num1,num2):\n",
+    "    print(\"product of 2 numbers\",num1*num2)\n",
+    "    \n",
+    "def division(num1,num2):\n",
+    "    print(\"quotient of 2 numbers is\",num1/num2)\n",
+    "    \n",
+    "    \n",
+    "num1=int(input(\"enter first number\"))\n",
+    "num2=int(input(\"enter second number\"))\n",
+    "addition(num1,num2)\n",
+    "subtraction(num1,num2)\n",
+    "multiplication(num1,num2)\n",
+    "division(num1,num2)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1299f85a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
